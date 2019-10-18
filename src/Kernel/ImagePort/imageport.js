462911f41515ef5c84c686e678228c32405c2028
kelvinKey <kelvinkeyss@yahoo.com>
2019-10-18 17:54:53 +0800

    init：imageport

--- src/Kernel/ImagePort/imageport.js
+++ src/Kernel/ImagePort/imageport.js
@@ -11,62 +11,40 @@ const config = require(__dirname.split('src')[0] + 'config.json');
 const port = {
 
     //新增永久素材
-    getMaterialImgUrl: ($token) => {
+    getMaterialImgUrl: async $token => {
 
         console.log(`$token:` + $token);
 
         return new Promise((resolve, reject) => {
 
             let formData = { media: port.WallpaperPicture() };
-            let options = { method: 'POST', url: util.format(config.apiURL.getImgUrl, config.prefix, $token), formData: formData, json: true };
+            let options = { method: 'POST', url: util.format(config.permanent.getImgUrl, config.prefix, $token), formData: formData, json: true };
 
             port.httpRequest(options, 'getMaterialImgUrl').then(data => {
-
-                port.fetchMaterial('image', data.media_id, {}, $token).then(res => {
-
-                    console.log('--------------------')
-                    console.log(res)
-                    resolve(res);
-                }).catch(err => {
-                    reject(err);
-                });
-
-            }).catch(err => {
-                reject(err);
+                return data;
             });
 
-            // port.uploadMaterial('image', port.WallpaperPicture(), {}, $token).then(data => {
-            //     console.log(data)
-            //     resolve(data);
-            // }).catch(err => {
-            //     reject(err);
-            // });
         });
 
     },
 
-    /**上传素材的方法 */
-    uploadMaterial: (type, material, permanent, token) => {
+    /**获取永久素材*/
+    fetchMaterial: (type, mediaId, permanent, token) => {
 
         return new Promise((resolve, reject) => {
-
-            let formData = { media: material, access_token: token };
-            let options = { method: 'POST', url: util.format(config.apiURL.upload, config.prefix, token), formData: formData, json: true };
-
-            port.httpRequest(options, 'uploadMaterial').then(data => {
+            let from = { media_id: mediaId }
+            let options = { method: 'POST', url: util.format(config.permanent.fetch, config.prefix, token), body: from, json: true };
+            console.log(options)
+            port.httpRequest(options, 'fetchMaterial').then(data => {
                 console.log(data)
-                port.fetchMaterial('image', data.media_id, {}, token).then(res => {
-
-                    console.log(res)
-                    resolve(res);
-                }).catch(err => {
-                    reject(err);
-                });
+                resolve(data)
             }).catch(err => {
                 reject(err);
             });
-        })
+
+        });
     },
+
     /**获取图片 */
     WallpaperPicture: () => {
         bluebird({ url: config.bing, json: true }).then(data => {
@@ -80,24 +58,6 @@ const port = {
             console.log(err);
         });
         return fs.createReadStream(path.join(__dirname, './img/image.jpg'));
-    },
-
-    /**获取素材的方法 */
-    fetchMaterial: (type, mediaId, permanent, token) => {
-
-        return new Promise((resolve, reject) => {
-            let from = { media_id: mediaId }
-
-            let options = { method: 'POST', url: util.format(config.apiURL.fetch, config.prefix, token), body: from, json: true };
-            console.log(options)
-            port.httpRequest(options, 'fetchMaterial').then(data => {
-                console.log(data)
-                resolve(data)
-            }).catch(err => {
-                reject(err);
-            });
-
-        });
     },
 
     //http处理函数
--- src/Kernel/Messagemanage/message.js
+++ src/Kernel/Messagemanage/message.js
@@ -1,63 +1,87 @@
-'use strict'
+
 
 const imageport = require('../ImagePort/imageport')
 
 const util = {
     NowTime: new Date().getTime(),
     reply: async ($message, $token) => {
-        let replys = '<xml>' +
-            '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
-            '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
-            '<CreateTime>' + util.NowTime + '</CreateTime>';
+
+        let replys = '';
 
         if ($message.MsgType == "event") {
+            replys = '';
             if ($message.Event === 'subscribe') { //订阅
 
                 $message.EventKey ? console.log('扫码进入: ' + $message.EventKey + ' ' + $message.Ticket) : console.log('添加进入');
 
-                replys += '<MsgType><![CDATA[text]]></MsgType>' +
+                replys = '<MsgType><![CDATA[text]]></MsgType>' +
                     '<Content><![CDATA[能再晚点吗，现在才关注我？\n这应该是全中国最糟糕的公众号，\n 我以捜罗全球创意领域的奇葩和 \n艺术界的败笔为乐趣，在毁灭你的 \n人生观、价值观同时向世界传递我 \n的虚情假意。\n(个人微信号：Sambay1226)\n回复神奇数字【1】\n开始吐糟→]]></Content>' +
                     '</xml>';
 
             } else if ($message.Event === 'unsubscribe') { //取消订阅
 
-                replys += '<MsgType><![CDATA[text]]></MsgType>' +
+                replys = '<xml>' +
+                    '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
+                    '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
+                    '<CreateTime>' + util.NowTime + '</CreateTime>' +
+                    '<MsgType><![CDATA[text]]></MsgType>' +
                     '<Content><![CDATA[]]></Content>' +
                     '</xml>';
 
             } else if ($message.Event === 'SCAN') {//扫描
                 console.log('看到你扫了一下奥: ' + $message.EventKey + $message.Ticket);
 
-                replys += '<MsgType><![CDATA[text]]></MsgType>' +
+                replys = '<xml>' +
+                    '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
+                    '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
+                    '<CreateTime>' + util.NowTime + '</CreateTime>' +
+                    '<MsgType><![CDATA[text]]></MsgType>' +
                     '<Content><![CDATA[你扫了我公众号的二维码奥~]]></Content>' +
                     '</xml>';
             }
 
         } else if ($message.MsgType == "text") {
             if ($message.Content == 1) {
-                replys += '<MsgType><![CDATA[' + $message.MsgType + ']]></MsgType>' +
+                replys = '<xml>' +
+                    '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
+                    '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
+                    '<CreateTime>' + util.NowTime + '</CreateTime>' +
+                    '<MsgType><![CDATA[' + $message.MsgType + ']]></MsgType>' +
                     '<Content><![CDATA[哇瑟~这么乖,尝试点点其他的数字奥~~~]]></Content>' +
                     '</xml>';
             } else if ($message.Content == 2) {
 
                 imageport.getMaterialImgUrl($token).then(data => {
+                    //TODO
+                });
 
-                    replys += '<MsgType><![CDATA[image]]></MsgType>' +
-                        '<Image>' +
-                        '<MediaId><![CDATA[' + data.media_id + ']]></MediaId>' +
-                        '</Image>' +
-                        '</xml>';
-                })
+                replys = '<xml>' +
+                    '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
+                    '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
+                    '<CreateTime>' + util.NowTime + '</CreateTime>' +
+                    '<MsgType><![CDATA[image]]></MsgType>' +
+                    '<Image>' +
+                    '<MediaId><![CDATA[' + 1 + ']]></MediaId>' +
+                    '</Image>' +
+                    '</xml>';
 
-
+                console.log(replys)
             } else {
-                replys += '<MsgType><![CDATA[' + $message.MsgType + ']]></MsgType>' +
+                replys = '<xml>' +
+                    '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
+                    '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
+                    '<CreateTime>' + util.NowTime + '</CreateTime>' +
+                    '<MsgType><![CDATA[' + $message.MsgType + ']]></MsgType>' +
                     '<Content><![CDATA[嘤嘤嘤~~不知道客官在讲什么奥~~~]]></Content>' +
                     '</xml>';
             }
         } else if ($message.MsgType == "image") {//图片
 
-            replys += '<MsgType><![CDATA[text]]></MsgType>' +
+            replys = '<xml>' +
+                '<ToUserName><![CDATA[' + $message.FromUserName + ']]></ToUserName>' +
+                '<FromUserName><![CDATA[' + $message.ToUserName + ']]></FromUserName>' +
+                '<CreateTime>' + util.NowTime + '</CreateTime>' +
+                '<MsgType><![CDATA[text]]></MsgType>' +
                 '<Content><![CDATA[哇~~~ 你的审美有点东西奥~]]></Content>' +
                 '</xml>';
         } else if ($message.MsgType == "voice") {
